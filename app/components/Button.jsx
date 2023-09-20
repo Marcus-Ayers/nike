213a6f006bd48913045fbdff7245a1d0f203d1f9
Marcus-Ayers <marcayers27@gmail.com>
2023-09-20 17:24:53 -0400

    landing with images

---
+++ app/components/Button.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const Button = ({ label, iconUrl }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+      {label}
+      <Image
+        src={iconUrl}
+        alt="arrow right icon"
+        className="ml-2 rounded-full h-5 w-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
