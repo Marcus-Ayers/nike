590b0d7a4d58155282bf0961dba3a9f1d47f026e
Marcus-Ayers <marcayers27@gmail.com>
2023-09-21 10:36:17 -0400

    special offer and button changes

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,14 +1,29 @@
 import Image from "next/image";
 
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
+
       {iconUrl && (
         <Image
           src={iconUrl}
           alt="arrow right icon"
-          className="ml-2 rounded-full h-5 w-5"
+          className="ml-2 rounded-full bg-white w-5 h-5"
         />
       )}
     </button>
